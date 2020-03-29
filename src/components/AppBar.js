57c4ed56d4076fb08593d58aadf2f1f8a1899d67
Kal <46393179+kalvin807@users.noreply.github.com>
2020-03-29 22:10:13 +0800

    Site do not show correct metadata

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,11 +1,9 @@
-import React from 'react'
-
+import AboutButton from './AboutButton'
 import AppBar from '@material-ui/core/AppBar'
-import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-
-import AboutButton from './AboutButton'
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,7 +22,7 @@ const AppBarMenu = () => {
       <AppBar color="primary" position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
-            Awesome Tools versus COVID-19{' '}
+            Awesome Tools aganist COVID-19{' '}
             <span aria-label="Fight on" role="img">
               💪
             </span>
