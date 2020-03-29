dbcfb3fe7a4072a1b4855896a166067f6c4b9040
Kal <46393179+kalvin807@users.noreply.github.com>
2020-03-29 22:13:18 +0800

    BUGFIX: Site do not show correct metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,29 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Awesome-tools-against-COVID-19`,
-    description: `A portal for all awesome ongoing COVID-19 OSS projects`,
-    author: `@kalvin807`,
+    title: 'Awesome Tools against COVID-19',
+    description: 'A portal for all awesome ongoing COVID-19 OSS projects',
+    author: '@kalvin807',
+    url: 'https://kalvin807.github.io/awesome-tools-vs-covid',
+    image: '/favicon/favicon.ico',
   },
   pathPrefix: '/awesome-tools-vs-covid',
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-material-ui`],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-material-ui',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Awesome Tools aganist COVID-19 `,
+        short_name: `Tools aganist COVID-19 `,
+        start_url: `/`,
+        background_color: `#1a237e`,
+        theme_color: `#1a237e`,
+        display: `standalone`,
+        icon: `static/favicon/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
 }
--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,68 +1,61 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import { StaticQuery, graphql } from 'gatsby'
 
-import { graphql, useStaticQuery } from 'gatsby'
-
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import React from 'react'
 
-function SEO({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
+const SEO = ({ title, description, image }) => (
+  <StaticQuery
+    query={query}
+    render={({
+      site: {
+        siteMetadata: {
+          defaultTitle,
+          titleTemplate,
+          defaultDescription,
+          siteUrl,
+          defaultImage,
+        },
+      },
+    }) => {
+      const seo = {
+        title: title || defaultTitle,
+        description: description || defaultDescription,
+        image: `${siteUrl}${image || defaultImage}`,
+        url: `${siteUrl}`,
       }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `viewport`,
-          content: `minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no`,
-        },
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-      ].concat(meta)}
-    />
-  )
-}
+      return (
+        <>
+          <Helmet title={seo.title} titleTemplate={titleTemplate}>
+            <meta name="description" content={seo.description} />
+            <meta name="image" content={seo.image} />
+            {seo.url && <meta property="og:url" content={seo.url} />}
+            {seo.title && <meta property="og:title" content={seo.title} />}
+            {seo.description && (
+              <meta property="og:description" content={seo.description} />
+            )}
+            {seo.image && <meta property="og:image" content={seo.image} />}
+          </Helmet>
+        </>
+      )
+    }}
+  />
+)
+export default SEO
 
 SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
+  title: null,
+  description: null,
+  image: null,
 }
-export default SEO
+const query = graphql`
+  query SEO {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        defaultDescription: description
+        siteUrl: url
+        defaultImage: image
+      }
+    }
+  }
+`
