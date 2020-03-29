e900445ceebe9bdc60584ae3c9768f3e3a8fe253
Calvin Leung <kalvin807@gmail.com>
2020-03-29 22:06:51 +0800

    Site do not show correct siteMetaData - Add missing meta data - Add related plugins for adding metadata - Add static icon file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import { ThemeProvider } from '@material-ui/core/styles'
-import theme from '../theme/siteTheme'
 import App from './App'
 import Layout from '../components/Layout'
+import React from 'react'
+import SEO from '../components/SEO'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from '../theme/siteTheme'
 
 export default () => (
   <ThemeProvider theme={theme}>
+    <SEO />
     <Layout>
       <App />
     </Layout>
